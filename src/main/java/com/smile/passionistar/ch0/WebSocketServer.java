cc08207c68365ebf38c314d73acc9793b10cad43
한상현 <big0258@naver.com>
2018-02-06 13:06:56 +0900

    180206 core1

--- src/main/java/com/smile/passionistar/ch0/WebSocketServer.java
+++ src/main/java/com/smile/passionistar/ch0/WebSocketServer.java
@@ -42,7 +42,7 @@ public final class WebSocketServer {
             sslCtx = null;
         }
 
-        EventLoopGroup bossGroup = new EpollEventLoopGroup(4);
+        EventLoopGroup bossGroup = new EpollEventLoopGroup(1);
         EventLoopGroup workerGroup = new EpollEventLoopGroup();
         try {
             ServerBootstrap b = new ServerBootstrap();
