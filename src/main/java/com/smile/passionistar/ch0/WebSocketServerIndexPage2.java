6fd7a5b23f8b3a1e12b0f522483d3abc7a8e614f
한상현 <big0258@naver.com>
2018-02-19 10:37:42 +0900

    180219 test

--- src/main/java/com/smile/passionistar/ch0/WebSocketServerIndexPage2.java
+++ src/main/java/com/smile/passionistar/ch0/WebSocketServerIndexPage2.java
@@ -1,23 +1,8 @@
-/*
- * Copyright 2012 The Netty Project
- *
- * The Netty Project licenses this file to you under the Apache License,
- * version 2.0 (the "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at:
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations
- * under the License.
- */
+
 package com.smile.passionistar.ch0;
 
 import io.netty.buffer.ByteBuf;
-import io.netty.buffer.Unpooled;
-import io.netty.util.CharsetUtil;
+import io.netty.buffer.PooledByteBufAllocator;
 
 /**
  * Generates the demo HTML page which is served at http://localhost:8080/
@@ -25,8 +10,7 @@ import io.netty.util.CharsetUtil;
 public final class WebSocketServerIndexPage2 {
 
     public static ByteBuf getContent(String webSocketLocation) {
-        return Unpooled.copiedBuffer(
-                " ", CharsetUtil.US_ASCII);
+        return PooledByteBufAllocator.DEFAULT.directBuffer(256);
     }
 
     private WebSocketServerIndexPage2() {
